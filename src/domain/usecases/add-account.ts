2e49b5d6365892a303facf90bfa5de373367fb87
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-06 16:18:13 -0300

    feat: ensure call addAccount with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,11 @@
+import { IAccount } from "../protocols/account"
+
+export interface IAddAccount {
+  add(account: IAccountModel): Promise<IAccount>
+}
+
+export interface IAccountModel {
+  name: string
+  email: string
+  password: string
+}
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,16 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { IValidation } from '../../protocols/validation'
+import { IAddAccount } from '../../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
-  constructor(private readonly validation: IValidation) {}
+  constructor(
+    private readonly validation: IValidation,
+    private readonly addAccount: IAddAccount
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body)
       if (error) {
         return new Promise((resolve) => resolve(badRequest(error)))
       }
+
+      const { confirmPassword, ...account } = httpRequest.body
+
+      await this.addAccount.add(account)
 
       return new Promise((resolve) => resolve(badRequest(new Error())))
     } catch (err) {
