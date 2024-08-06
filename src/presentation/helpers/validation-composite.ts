b138c5c739a66d7cbb0b8eabd67a0d152bd136cc
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-06 16:56:36 -0300

    feat: ensure ValidationComposite call validation with correct value

---
+++ src/presentation/helpers/validation-composite.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IValidation } from "../protocols/validation";
+
+export class ValidationComposite implements IValidation {
+  constructor(private readonly validations: IValidation[]) {}
+  validate(data: any): Error | null {
+    for (const validation of this.validations) {
+      const error = validation.validate(data)
+      if (error) {
+        return error
+      }
+    }
+    return null
+  }
+}
