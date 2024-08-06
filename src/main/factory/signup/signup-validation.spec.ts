862ad9bf6acf0fbbdeb2a087117180b620cdf85c
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-06 17:39:07 -0300

    test: ensure SignUpValidation call ValidationComposite with correct values

--- src/main/factory/signup/signup-validation.spec.ts
+++ src/main/factory/signup/signup-validation.spec.ts
@@ -1,3 +1,4 @@
+import { RequiredFieldValidation } from '../../../presentation/helpers/validations/required-field-validation'
 import { ValidationComposite } from '../../../presentation/helpers/validations/validation-composite'
 import { IValidation } from '../../../presentation/protocols/validation'
 import { makeSignUpValidation } from './signup-validation'
@@ -8,6 +9,9 @@ describe('SignUp Validations', () => {
   it('Should call ValidationComposite with correct values', async () => {
     makeSignUpValidation()
     const validations: IValidation[] = []
+    for (const pos of ['name', 'email', 'password', 'confirmPassword']) {
+      validations.push(new RequiredFieldValidation(pos))
+    }
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
