d248add00cdb2840f0439765adc63e05200f6b15
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-06 17:37:39 -0300

    feat: ensure SignUpValidation call ValidationComposite with correct values

--- src/main/factory/signup/signup-validation.ts
+++ src/main/factory/signup/signup-validation.ts
@@ -1,8 +1,11 @@
+import { RequiredFieldValidation } from "../../../presentation/helpers/validations/required-field-validation";
 import { ValidationComposite } from "../../../presentation/helpers/validations/validation-composite";
 import { IValidation } from "../../../presentation/protocols/validation";
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: IValidation[] = []
-  
+  for (const pos of ['name', 'email', 'password', 'confirmPassword']) {
+    validations.push(new RequiredFieldValidation(pos))
+  }
   return new ValidationComposite(validations)
 }
