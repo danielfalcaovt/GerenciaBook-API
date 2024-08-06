598502d3abc931dc2e9340a5b3afaec2f1ef85fc
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-06 15:27:00 -0300

    chore: prettier add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,6 @@ export default [
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {extends: ["eslint:recommended", "plugin:prettier/recommended"]},
+  {rules: {"prettier/prettier": "error"}}
 ];
