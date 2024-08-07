bc83f8b0439c1b59aa4bdabe6518fc21a0bfcd94
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-07 13:56:08 -0300

    test: ensure SignUpValidation call ValidationComposite with correct values

--- src/main/factory/signup/signup-validation.spec.ts
+++ src/main/factory/signup/signup-validation.spec.ts
@@ -1,6 +1,8 @@
+import { EmailValidation } from '../../../presentation/helpers/validations/email-validation'
 import { RequiredFieldValidation } from '../../../presentation/helpers/validations/required-field-validation'
 import { ValidationComposite } from '../../../presentation/helpers/validations/validation-composite'
 import { IValidation } from '../../../presentation/protocols/validation'
+import { EmailValidator } from '../../../utils/email-validator/email-validator'
 import { makeSignUpValidation } from './signup-validation'
 
 jest.mock('../../../presentation/helpers/validations/validation-composite')
@@ -12,6 +14,7 @@ describe('SignUp Validations', () => {
     for (const pos of ['name', 'email', 'password', 'confirmPassword']) {
       validations.push(new RequiredFieldValidation(pos))
     }
+    validations.push(new EmailValidation(new EmailValidator()))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
