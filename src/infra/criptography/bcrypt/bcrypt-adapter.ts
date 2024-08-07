5cf286ccc02397ed6138f10010d56af30ff867f0
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-07 14:31:14 -0300

    feat: ensure BcryptAdapter call hash with correct values

---
+++ src/infra/criptography/bcrypt/bcrypt-adapter.ts
@@ -0,0 +1,10 @@
+import bcrypt from "bcrypt";
+import { IHasher } from "../../../data/protocols/cryptography/ihasher";
+
+export class BcryptAdapter implements IHasher {
+  constructor(private readonly salt: number) {}
+  async hash(value: string): Promise<string> {
+    const hashedValue = await bcrypt.hash(value, this.salt)
+    return hashedValue
+  }
+}
