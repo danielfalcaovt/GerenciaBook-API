c5c82e2398abfc530afaf5bb4f14d4b545c23b42
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-12 15:43:54 -0300

    --amend

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import bodyParser from '../middlewares/body-parser/body-parser'
 import contentType from '../middlewares/body-parser/content-type'
+import cors from '../middlewares/body-parser/cors'
 
 export default (app: Express) => {
   app.use(bodyParser)
   app.use(contentType)
+  app.use(cors)
 }
