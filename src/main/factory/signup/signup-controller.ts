f80897ee335b5994d6c3babb314aa0ee548a9aa0
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-12 14:34:46 -0300

    feat: ensure return an account on call signup route

--- src/main/factory/signup/signup-controller.ts
+++ src/main/factory/signup/signup-controller.ts
@@ -1,12 +1,14 @@
 import { DbAddAccount } from "../../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../../infra/criptography/bcrypt/bcrypt-adapter";
+import { MongoAccountRepository } from "../../../infra/db/mongodb/account-repository/account-repository";
 import { SignUpController } from "../../../presentation/controllers/signup/signup";
 import { makeSignUpValidation } from "./signup-validation";
 
 export const makeSignUpController = (): SignUpController => {
   const salt = 12
   const hasher = new BcryptAdapter(salt)
-  const addAccount = new DbAddAccount(hasher, )
+  const AddAccountRepository = new MongoAccountRepository()
+  const addAccount = new DbAddAccount(hasher, AddAccountRepository)
   const signUpController = new SignUpController(makeSignUpValidation(), addAccount)
   return signUpController
 }
