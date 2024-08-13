c6d17d25819f9ee38c5abe8d2a436a729ba3f6ea
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-13 18:42:56 -0300

    feat: ensure return 500 if validation throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { IAuthentication } from "../../../domain/usecases/authentication";
-import { badRequest, ok } from "../../helpers/http-helper";
+import { badRequest, ok, serverError } from "../../helpers/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { IValidation } from "../../protocols/validation";
@@ -10,10 +10,16 @@ export class LoginController implements Controller {
     private readonly authentication: IAuthentication
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+    try {
+
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return new Promise(resolve => resolve(badRequest(error)))
+      }
+      return new Promise(resolve => resolve(ok({})))
+    } catch (err) {
+      console.error(err)
+      return serverError()
     }
-    return new Promise(resolve => resolve(ok({})))
   }
 }
