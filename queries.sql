5588aa56b401b1a043e3f8f0c04bc485186eb930
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-13 17:21:10 -0300

    docs: app configuration

--- queries.sql
+++ queries.sql
@@ -4,5 +4,6 @@ CREATE TABLE users (
   id uuid default gen_random_uuid() PRIMARY KEY,
   name varchar(100) NOT NULL,
   email varchar(100) NOT NULL,
-  password varchar(100) NOT NULL
+  password varchar(100) NOT NULL,
+  accessToken varchar(150)
 )
