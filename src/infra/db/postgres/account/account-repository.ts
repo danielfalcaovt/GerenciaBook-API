2d3dfc106d7817fb06c79de34790634fa0e585d5
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-14 14:34:36 -0300

    feat: ensure PgAccountRepository update clal query with correct values

--- src/infra/db/postgres/account/account-repository.ts
+++ src/infra/db/postgres/account/account-repository.ts
@@ -1,10 +1,11 @@
 import { IAddAccountRepository } from "../../../../data/protocols/db/iadd-account-repository";
 import { ILoadByEmail } from "../../../../data/protocols/db/iload-by-email";
+import { IUpdateAccessToken } from "../../../../data/protocols/db/iupdate-access-token";
 import { IAccount } from "../../../../domain/protocols/account";
 import { IAccountModel } from "../../../../domain/usecases/add-account";
 import { PgHelper } from "../helpers/pg-helper";
 
-export class PgAccountRepository implements IAddAccountRepository, ILoadByEmail {
+export class PgAccountRepository implements IAddAccountRepository, ILoadByEmail, IUpdateAccessToken {
   async add(account: IAccountModel): Promise<IAccount> {
     const insertedAccount = await PgHelper.query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *', [account.name, account.email, account.password])
     if (insertedAccount.rows.length > 0) {
@@ -19,5 +20,9 @@ export class PgAccountRepository implements IAddAccountRepository, ILoadByEmail 
       return new Promise(resolve => resolve(foundAccount.rows[0]))
     }
     return new Promise(resolve => resolve(null))
+  }
+
+  async update(id: string, token: string): Promise<void> {
+    await PgHelper.query('UPDATE users SET token = $1 WHERE id = $2', [token, id])
   }
 }
