1de818eb6aa4505e836dae35cd7cff857c76c45e
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-15 19:44:09 -0300

    test: ensure DbGetBooks call get

---
+++ src/data/usecases/books/get/db-get-books.spec.ts
@@ -0,0 +1,45 @@
+import { IBook } from '../../../../domain/protocols/book'
+import { IDbGetBooksRepository } from '../../../protocols/db/books/idb-get-books-repository'
+import { DbGetBooks } from './db-get-books'
+
+interface SutTypes {
+  sut: DbGetBooks
+  dbGetBooksRepositoryStub: IDbGetBooksRepository
+}
+
+const makeSut = (): SutTypes => {
+  const dbGetBooksRepositoryStub = makeDbGetBooksRepositoryStub()
+  const sut = new DbGetBooks(dbGetBooksRepositoryStub)
+  return {
+    sut,
+    dbGetBooksRepositoryStub
+  }
+}
+
+const makeFakeBook = (): IBook => ({
+  book_name: 'any_book',
+  id: 'any_id',
+  lend_day: 'any_day',
+  student_name: 'any_name'
+})
+
+const makeDbGetBooksRepositoryStub = (): IDbGetBooksRepository => {
+  class DbGetBooksRepositoryStub implements IDbGetBooksRepository {
+    get(): Promise<IBook[]> {
+      return new Promise(resolve => resolve([
+        makeFakeBook(),
+        makeFakeBook()
+      ]))
+    }
+  }
+  return new DbGetBooksRepositoryStub()
+}
+
+describe('DbGetBooks', () => {
+  it('Should call get', async () => {
+    const { sut, dbGetBooksRepositoryStub } = makeSut()
+    const getSpy = jest.spyOn(dbGetBooksRepositoryStub, 'get')
+    await sut.get()
+    expect(getSpy).toHaveBeenCalledTimes(1)
+  })
+})
