218bfaa095864f82025e08e180f685f9aba9ee40
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-15 19:24:23 -0300

    feat: ensure GetBooksController return 500 if getBooks throw

--- src/presentation/controllers/books/get-many/get-books-controller.ts
+++ src/presentation/controllers/books/get-many/get-books-controller.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { IGetBooks } from "../../../../domain/usecases/books/get/iget-books";
-import { Controller, HttpRequest, HttpResponse, ok } from "../books-protocols";
+import { Controller, HttpRequest, HttpResponse, ok, serverError } from "../books-protocols";
 
 export class GetBooksController implements Controller {
   constructor(private readonly getBooks: IGetBooks) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const result = await this.getBooks.get()
-    return new Promise(resolve => resolve(ok(result)))
+    try {
+      const result = await this.getBooks.get()
+      return new Promise(resolve => resolve(ok(result)))
+    } catch (err) {
+      return serverError()
+    }
   }
 }
