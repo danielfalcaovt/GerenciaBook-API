dd6041969f165a097bda1417ad143e1f1b201f9a
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-15 19:20:14 -0300

    feat: ensure GetBooksController return getBooks result

--- src/presentation/controllers/books/get-many/get-books-controller.ts
+++ src/presentation/controllers/books/get-many/get-books-controller.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { IGetBooks } from "../../../../domain/usecases/books/get/iget-books";
-import { Controller, HttpRequest, HttpResponse } from "../../login/login-protocols";
+import { Controller, HttpRequest, HttpResponse, ok } from "../books-protocols";
 
 export class GetBooksController implements Controller {
   constructor(private readonly getBooks: IGetBooks) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.getBooks.get()
-    return new Promise(resolve => resolve({statusCode:200}))
+    const result = await this.getBooks.get()
+    return new Promise(resolve => resolve(ok(result)))
   }
 }
