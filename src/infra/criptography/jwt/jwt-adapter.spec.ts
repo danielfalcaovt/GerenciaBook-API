2f93c71000b343fffa12c026205d395953c12aa0
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-15 14:41:09 -0300

    test: ensure JwtAdapter return a token on succeed

--- src/infra/criptography/jwt/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -6,11 +6,22 @@ const makeSut = (): JwtAdapter => {
   return sut
 }
 
+jest.mock('jsonwebtoken', () => ({
+  sign() {
+    return 'any_token'
+  }
+}))
+
 describe('JwtAdapter', () => {
   it('Should call sign with correct values', async () => {
     const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.generate('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, expect.anything())
+  })
+  it('Should return a token on succeed', async () => {
+    const sut = makeSut()
+    const token = await sut.generate('any_id')
+    expect(token).toBe('any_token')
   })
 })
