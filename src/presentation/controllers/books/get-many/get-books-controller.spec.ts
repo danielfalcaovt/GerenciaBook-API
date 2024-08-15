6514aedde190809baa521222915f81454ef743d4
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-15 19:12:24 -0300

    test: ensure GetBooksController call GetBooks

---
+++ src/presentation/controllers/books/get-many/get-books-controller.spec.ts
@@ -0,0 +1,45 @@
+import { IBook } from '../../../../domain/protocols/book'
+import { IGetBooks } from '../../../../domain/usecases/books/get/iget-books'
+import { GetBooksController } from './get-books-controller'
+
+interface SutTypes {
+  sut: GetBooksController
+  getBooksStub: IGetBooks
+}
+
+const makeSut = (): SutTypes => {
+  const getBooksStub = makeGetBooksStub()
+  const sut = new GetBooksController(getBooksStub)
+  return {
+    sut,
+    getBooksStub
+  }
+}
+
+const makeFakeBook = (): IBook => ({
+  book_name: 'any_book',
+  id: 'any_id',
+  lend_day: 'any_day',
+  student_name: 'any_name'
+})
+
+const makeGetBooksStub = (): IGetBooks => {
+  class GetBooksStub implements IGetBooks {
+    get(): Promise<IBook[]> {
+      return new Promise(resolve => resolve([
+        makeFakeBook(),
+        makeFakeBook()
+      ]))
+    }
+  }
+  return new GetBooksStub()
+}
+
+describe('GetBooksController', () => {
+  it('Should call GetBooks', async () => {
+    const { sut, getBooksStub } = makeSut()
+    const getSpy = jest.spyOn(getBooksStub, 'get')
+    await sut.handle({})
+    expect(getSpy).toHaveBeenCalledTimes(1)
+  })
+})
