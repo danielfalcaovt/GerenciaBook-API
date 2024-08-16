0613039acb0310c5cf185ba5726c99bf5c0fa230
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-16 14:35:29 -0300

    test: ensure BooksRepository return an empty array if query fail

---
+++ src/infra/db/postgres/books/books-repository.test.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { IBook } from '../../../../domain/protocols/book'
+import { PgHelper } from '../helpers/pg-helper'
+import { BooksRepository } from './books-repository'
+
+const makeFakeBook = (): IBook => ({
+  book_name: 'any_book',
+  id: 'any_id',
+  lend_day: new Date(),
+  student_name: 'any_name',
+  student_class: 3001
+})
+
+describe('BooksRepository', () => {
+  beforeAll(async () => {
+    PgHelper.connect().then(() => {})
+  })
+
+  beforeEach(async () => {
+    PgHelper.query('DELETE FROM books').then(() => {})
+  })
+
+  afterAll(async () => {
+    PgHelper.disconnect().then(() => {})
+  })
+  it('Should call query with correct values', async () => {
+    const sut = new BooksRepository()
+    const querySpy = jest.spyOn(PgHelper, 'query')
+    await sut.get()
+    expect(querySpy).toHaveBeenCalledTimes(1)
+  })
+  it('Should return an empty array if query fail', async () => {
+    const sut = new BooksRepository()
+    jest.spyOn(PgHelper, 'query').mockImplementationOnce(() => {
+      return new Promise(resolve => resolve({ rows: [] } as any))
+    })
+    const result = await sut.get()
+    expect(result).toEqual([])
+  })
+})
