eb226d558636509ae78dabc6fc6124c76db74b65
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-16 18:26:33 -0300

    feat: ensure AuthMiddleware call TokenVerifier with correct value

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,13 +1,17 @@
+import { ITokenVerifier } from "../../data/protocols/cryptography/itoken-verifier";
 import { AccessDeniedError } from "../errors";
 import { forbidden, ok } from "../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { IMiddleware } from "../protocols/middleware";
 
 export class AuthMiddleware implements IMiddleware {
+  constructor (private readonly tokenVerifier: ITokenVerifier) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.headers.authorization) {
       return forbidden(new AccessDeniedError())
     }
+    await this.tokenVerifier.verify(httpRequest.headers.authorization)
     return new Promise(resolve => resolve(ok({})))
   }
 }
