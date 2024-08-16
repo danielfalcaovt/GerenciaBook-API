9cb19f5813e9ed481e72b67f47e090aebe4707ff
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-16 14:25:14 -0300

    feat: ensure BooksRepository call query with correct values

---
+++ src/infra/db/postgres/books/books-repository.ts
@@ -0,0 +1,14 @@
+import { IDbGetBooksRepository } from "../../../../data/protocols/db/books/idb-get-books-repository";
+import { IBook } from "../../../../domain/protocols/book";
+import { PgHelper } from "../helpers/pg-helper";
+
+export class BooksRepository implements IDbGetBooksRepository {
+  async get(): Promise<IBook[]> {
+    const result = await PgHelper.query('SELECT * FROM books')
+    if (result.rows.length > 0) {
+      return new Promise(resolve => resolve(result.rows))
+    } else {
+      return new Promise(resolve => resolve([]))
+    }
+  }
+}
