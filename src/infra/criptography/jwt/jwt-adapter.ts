6a4c97ae6783fc6fe2d993237d7d13c0d5958185
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-16 19:38:09 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -1,10 +1,17 @@
-import { ITokenGenerator } from "../../../data/protocols/cryptography/itoken-generator";
+ 
+import { ITokenGenerator } from '../../../data/protocols/cryptography/itoken-generator'
+import { ITokenVerifier } from '../../../data/protocols/cryptography/itoken-verifier'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements ITokenGenerator {
+export class JwtAdapter implements ITokenGenerator, ITokenVerifier {
   constructor(private readonly JWT_SECRET: string) {}
   async generate(id: string): Promise<string> {
     const token = jwt.sign({ id }, this.JWT_SECRET)
     return token
+  }
+
+  async verify(token: string): Promise<string | null> {
+    jwt.verify(token, this.JWT_SECRET)
+    return new Promise(resolve => resolve('any'))
   }
 }
