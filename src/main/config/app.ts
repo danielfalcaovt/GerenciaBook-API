0500759b0247a86b55ca039d90e6b67e6e1ec897
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-19 17:33:17 -0300

    feat: ensure AuthMiddleware return 403 if user is not authenticated

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
-import routes from './routes'
+import authRoutes from './auth-routes'
 import middlewares from './middlewares'
+import authMiddleware from '../middlewares/auth-middleware/auth-middleware'
 
 const app = express()
 
 middlewares(app)
-routes(app)
+authRoutes(app)
+app.use(authMiddleware)
+
 
 export default app
