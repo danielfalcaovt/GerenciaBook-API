b877a3a49a7bfd7b3a7c40cd890376b4206ce583
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-20 15:47:35 -0300

    test: ensure RequiredAtLeastOneIn not return if ok

--- src/presentation/helpers/validations/required-at-least-one-in.spec.ts
+++ src/presentation/helpers/validations/required-at-least-one-in.spec.ts
@@ -2,7 +2,8 @@ import { MissingParamError } from "../../errors"
 import { RequiredAtLeastOneIn } from './required-at-least-one-in'
 
 const makeFakeRequest = () => ({
-
+  paramOne: 'any_value',
+  paramTwo: 'any_value'
 })
 
 const makeFakeValidationParameters = () => ([
@@ -13,7 +14,12 @@ const makeFakeValidationParameters = () => ([
 describe('RequiredAtLeastOneIn', () => {
   it('Should return an error if no parameter was found', async () => {
     const sut = new RequiredAtLeastOneIn(makeFakeValidationParameters())
-    const error = sut.validate(makeFakeRequest())
+    const error = sut.validate({})
     expect(error).toEqual(new MissingParamError('required at least a param'))
+  })
+  it('Should not return if ok', async () => {
+    const sut = new RequiredAtLeastOneIn(makeFakeValidationParameters())
+    const result = sut.validate(makeFakeRequest())
+    expect(result).toBeFalsy()
   })
 })
