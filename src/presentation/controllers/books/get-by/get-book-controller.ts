422a2a3682f048c8606cd4fb3484a93a1aa583f6
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-20 14:38:33 -0300

    feat: ensure GetBookController return the GetBook result

--- src/presentation/controllers/books/get-by/get-book-controller.ts
+++ src/presentation/controllers/books/get-by/get-book-controller.ts
@@ -9,7 +9,7 @@ export class GetBookController implements Controller {
     if (error) {
       return new Promise(resolve => resolve(badRequest(error)))
     }
-    await this.GetBook.get(httpRequest.body)
-    return new Promise(resolve => resolve(ok({})))
+    const result = await this.GetBook.get(httpRequest.body)
+    return new Promise(resolve => resolve(ok(result)))
   }
 }
