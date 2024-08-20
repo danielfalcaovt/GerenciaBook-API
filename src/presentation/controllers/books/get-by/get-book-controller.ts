61090ffe8c3f7a9837fc1b95ac3672b99c9a43a6
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-20 14:21:44 -0300

    feat: ensure GetBookController return an error if validation fail

--- src/presentation/controllers/books/get-by/get-book-controller.ts
+++ src/presentation/controllers/books/get-by/get-book-controller.ts
@@ -1,11 +1,14 @@
 import { IGetBook } from "../../../../domain/usecases/books/get/iget-by-books";
-import { Controller, HttpRequest, HttpResponse, IValidation, ok } from "../books-protocols";
+import { badRequest, Controller, HttpRequest, HttpResponse, IValidation, ok } from "../books-protocols";
 
 export class GetBookController implements Controller {
   constructor(private readonly validation: IValidation, private readonly GetBook: IGetBook) {}
   
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return new Promise(resolve => resolve(badRequest(error)))
+    }
     return new Promise(resolve => resolve(ok({})))
   }
 }
