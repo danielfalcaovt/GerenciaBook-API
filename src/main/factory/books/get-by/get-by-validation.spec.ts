0e18683d276c76c4e5a30434de8cbe0d66a0bb2d
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-20 15:58:52 -0300

    test: ensure GetBy Validation call ValidationComposite with correct values

---
+++ src/main/factory/books/get-by/get-by-validation.spec.ts
@@ -0,0 +1,21 @@
+import { RequiredAtLeastOneIn } from '../../../../presentation/helpers/validations/required-at-least-one-in'
+import { IValidation } from '../../login/login-protocols'
+import { ValidationComposite } from '../../login/login-protocols'
+import { makeGetByValidation } from './get-by-validations'
+
+jest.mock('../../login/login-protocols')
+
+describe('GetBy Validations', () => {
+  it('Should call ValidationComposite with correct values', async () => {
+    makeGetByValidation()
+    const validations: IValidation[] = []
+    const params: string[] = [
+      'book_name',
+      'student_name',
+      'student_class',
+      'lend_day'
+    ]
+    validations.push(new RequiredAtLeastOneIn(params))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
