5c9a174e9599348e223682224a1d40f240b5f695
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-20 14:48:04 -0300

    feat: ensure GetBookController return 500 if GetBook throws

--- src/presentation/controllers/books/get-by/get-book-controller.ts
+++ src/presentation/controllers/books/get-by/get-book-controller.ts
@@ -1,15 +1,31 @@
-import { IGetBook } from "../../../../domain/usecases/books/get/iget-by-books";
-import { badRequest, Controller, HttpRequest, HttpResponse, IValidation, ok } from "../books-protocols";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { IGetBook } from '../../../../domain/usecases/books/get/iget-by-books'
+import {
+  badRequest,
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  IValidation,
+  ok,
+  serverError
+} from '../books-protocols'
 
 export class GetBookController implements Controller {
-  constructor(private readonly validation: IValidation, private readonly GetBook: IGetBook) {}
-  
+  constructor(
+    private readonly validation: IValidation,
+    private readonly GetBook: IGetBook
+  ) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return new Promise((resolve) => resolve(badRequest(error)))
+      }
+      const result = await this.GetBook.get(httpRequest.body)
+      return new Promise((resolve) => resolve(ok(result)))
+    } catch (err) {
+      return serverError()
     }
-    const result = await this.GetBook.get(httpRequest.body)
-    return new Promise(resolve => resolve(ok(result)))
   }
 }
