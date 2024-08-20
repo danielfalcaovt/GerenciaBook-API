7ec06d8be5dac195666bf1748ac6155521fc3608
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-20 19:58:17 -0300

    test: ensure GetBy Route return 200 on succeed

--- src/main/routes/books/get/get-by-routes.test.ts
+++ src/main/routes/books/get/get-by-routes.test.ts
@@ -1,9 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import request from 'supertest'
 import app from '../../../config/app'
+import { NextFunction } from 'express'
+import { PgHelper } from '../../../../infra/db/postgres/helpers/pg-helper'
+
+jest.mock('../../../middlewares/auth-middleware/auth-middleware', () => jest.fn((req: any, res: any, next: NextFunction) => next()))
 
 describe('GetBy Route', () => {
+  beforeAll(async () => {
+    PgHelper.connect().then(() => {})
+  })
+
+  beforeEach(async () => {
+    PgHelper.query('DELETE FROM books').then(() => {})
+  })
+
+  afterAll(async () => {
+    PgHelper.disconnect().then(() => {})
+  })
+
   it('Should return 200 on succeed', async () => {
     await request(app)
-      .get('/api/get/')
+      .get('/api/books/query?student_name=any_name')
+      .expect(200)
   })
 })
