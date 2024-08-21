676755d0b35d197bb075fbc6fb64e973af522710
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-21 13:59:16 -0300

    feat: ensure AddBookController return 500 if validate throws

--- src/presentation/controllers/books/post/add-book-controller.ts
+++ src/presentation/controllers/books/post/add-book-controller.ts
@@ -1,5 +1,5 @@
 import { IAddBook } from "../../../../domain/usecases/books/post/iadd-book";
-import { badRequest, Controller, HttpRequest, HttpResponse, IValidation, ok } from "../books-protocols";
+import { badRequest, Controller, HttpRequest, HttpResponse, IValidation, ok, serverError } from "../books-protocols";
 
 export class AddBookController implements Controller {
   constructor(
@@ -7,10 +7,15 @@ export class AddBookController implements Controller {
     private readonly addBook: IAddBook
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return new Promise(resolve => resolve(badRequest(error)))
+      }
+      return new Promise(resolve => resolve(ok({})))
+    } catch (err) {
+      console.log(err)
+      return new Promise(resolve => resolve(serverError()))
     }
-    return new Promise(resolve => resolve(ok({})))
   }
 }
