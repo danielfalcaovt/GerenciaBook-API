41e48b0a7f3b9cbcf01ccc9cd02edbfaf384b1fd
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-21 14:06:25 -0300

    refactor: remove duplicated code

--- src/main/routes/books/get/get-by-routes.ts
+++ src/main/routes/books/get/get-by-routes.ts
@@ -3,6 +3,5 @@ import routeAdapter from '../../../adapters/route-adapter'
 import { makeGetByController } from '../../../factory/books/get-by/get-by-controller'
 
 export default (router: Router) => {
-  const bookController = makeGetByController()
-  router.get('/books/query', routeAdapter(bookController))
+  router.get('/books/query', routeAdapter(makeGetByController()))
 }
