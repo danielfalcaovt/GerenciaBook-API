eda446a55f91fc0da652af6e7a32730709c06895
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-22 14:04:08 -0300

    test: ensure DbAddBook call post with correct values

---
+++ src/data/usecases/books/post/db-add-book.spec.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { IBook } from '../../../../domain/protocols/book'
+import { IAddBookModel } from '../../../../domain/usecases/books/post/idb-add-book'
+import { DbAddBook } from './db-add-book'
+import { IDbAddBookRepository } from '../../../protocols/db/books/idb-add-books-repository'
+
+interface SutTypes {
+  sut: DbAddBook
+  addBookRepositoryStub: IDbAddBookRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addBookRepositoryStub = makeAddBookRepositoryStub()
+  const sut = new DbAddBook(addBookRepositoryStub)
+  return {
+    sut, 
+    addBookRepositoryStub
+  }
+}
+
+const makeAddBookRepositoryStub = (): IDbAddBookRepository => {
+  class AddBookRepositoryStub implements IDbAddBookRepository {
+    add(book: IAddBookModel): Promise<IBook> {
+      return new Promise(resolve => resolve({
+        book_name: 'any_book',
+        id: 'any_id',
+        lend_day: 1238128930128,
+        student_class: 3000,
+        student_name: 'any_name'
+      }))
+    }
+  }
+  return new AddBookRepositoryStub()
+}
+
+const makeFakeRequest = (): IAddBookModel => ({
+  book_name: 'any_book',
+  lend_day: 1238128930128,
+  student_class: 3000,
+  student_name: 'any_name'
+})
+
+describe('DbAddBook', () => {
+  it('Should call post with correct values', async () => {
+    const { sut, addBookRepositoryStub } = makeSut()
+    const addSpyRep = jest.spyOn(addBookRepositoryStub, 'add')
+    await sut.add(makeFakeRequest())
+    expect(addSpyRep).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
