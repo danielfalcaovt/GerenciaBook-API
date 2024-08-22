323f6456ba25310e0ffb47af51474d3729b4d9fb
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-22 13:15:45 -0300

    fix: jwt expires in 8h add

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken'
 export class JwtAdapter implements ITokenGenerator, ITokenVerifier {
   constructor(private readonly JWT_SECRET: string) {}
   async generate(id: string): Promise<string> {
-    const token = jwt.sign({ id }, this.JWT_SECRET)
+    const token = jwt.sign({ id }, this.JWT_SECRET, { expiresIn: '8h' })
     return token
   }
 
