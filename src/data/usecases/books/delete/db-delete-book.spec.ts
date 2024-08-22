d2107b9bb5be32e4901ec874a40cdf77152d1317
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-22 17:19:19 -0300

    test: ensure DbDeleteBook call DeleteBookRepository with correct values

---
+++ src/data/usecases/books/delete/db-delete-book.spec.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { IDbDeleteBookRepository } from '../../../protocols/db/books/idb-delete-books-repository'
+import { DbDeleteBook } from './db-delete-book'
+
+interface SutTypes {
+  sut: DbDeleteBook
+  deleteBookRepositoryStub: IDbDeleteBookRepository
+}
+
+const makeSut = (): SutTypes => {
+  const deleteBookRepositoryStub = makeDeleteBookRepositoryStub()
+  const sut = new DbDeleteBook(deleteBookRepositoryStub)
+  return {
+    sut,
+    deleteBookRepositoryStub
+  }
+}
+
+const makeDeleteBookRepositoryStub = (): IDbDeleteBookRepository => {
+  class DbDeleteBookRepositoryStub implements IDbDeleteBookRepository {
+      delete(bookId: string): Promise<number> {
+        return new Promise(resolve => resolve(1))
+      }
+  }
+  return new DbDeleteBookRepositoryStub()
+}
+
+describe('DbDeleteBook', () => {
+  it('Should call delete with correct values', async () => {
+    const { sut, deleteBookRepositoryStub } = makeSut()
+    const deleteSpy = jest.spyOn(deleteBookRepositoryStub, 'delete')
+    await sut.delete('any_id')
+    expect(deleteSpy).toHaveBeenCalledWith('any_id')
+  })
+})
