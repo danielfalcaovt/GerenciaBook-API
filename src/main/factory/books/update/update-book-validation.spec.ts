81bc5a4b6db11dd4507ba63d845726fdbb5a25ac
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-26 16:00:58 -0300

    test: ensure UpdateBookValidation call ValidationComposite with correct values

---
+++ src/main/factory/books/update/update-book-validation.spec.ts
@@ -0,0 +1,21 @@
+import { RequiredAtLeastOneIn } from '../../../../presentation/helpers/validations/required-at-least-one-in'
+import { makeUpdateBookValidation } from './update-book-validations'
+import { IValidation, RequiredFieldValidation, ValidationComposite } from './update-protocols'
+
+jest.mock('../../../../presentation/helpers/validations/validation-composite')
+
+describe('updateBook Validations', () => {
+  it('Should call ValidationComposite with correct values', async () => {
+    makeUpdateBookValidation()
+    const validations: IValidation[] = []
+    validations.push(new RequiredFieldValidation('id'))
+    const params: string[] = [
+      'book_name',
+      'student_name',
+      'student_class',
+      'lend_day'
+    ]
+    validations.push(new RequiredAtLeastOneIn(params))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
