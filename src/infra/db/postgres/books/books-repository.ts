88f49a5394acf2b2fd5223fa8db05918553587f2
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-26 14:53:02 -0300

    feat: ensure UpdateBookRepository call query with correct values

--- src/infra/db/postgres/books/books-repository.ts
+++ src/infra/db/postgres/books/books-repository.ts
@@ -3,12 +3,14 @@ import { IDbAddBookRepository } from "../../../../data/protocols/db/books/idb-ad
 import { IDbDeleteBookRepository } from "../../../../data/protocols/db/books/idb-delete-books-repository";
 import { IDbGetBookRepository } from "../../../../data/protocols/db/books/idb-get-books-repository";
 import { IDbGetByBookRepository } from "../../../../data/protocols/db/books/idb-get-by-books";
+import { IDbUpdateBookRepository } from "../../../../data/protocols/db/books/idb-update-books-repository";
 import { IBook } from "../../../../domain/protocols/book";
 import { IGetBookModel } from "../../../../domain/usecases/books/get/iget-by-books";
 import { IAddBookModel } from "../../../../domain/usecases/books/post/idb-add-book";
+import { IUpdateBookModel } from "../../../../domain/usecases/books/update/iupdate-by-books";
 import { PgHelper } from "../helpers/pg-helper";
 
-export class BooksRepository implements IDbGetBookRepository, IDbGetByBookRepository, IDbAddBookRepository, IDbDeleteBookRepository {
+export class BooksRepository implements IDbGetBookRepository, IDbGetByBookRepository, IDbAddBookRepository, IDbDeleteBookRepository, IDbUpdateBookRepository {
   async get(): Promise<IBook[]> {
     const result = await PgHelper.query('SELECT * FROM books')
     if (result.rows.length > 0) {
@@ -75,5 +77,43 @@ export class BooksRepository implements IDbGetBookRepository, IDbGetByBookReposi
     } else {
       return new Promise(resolve => resolve(0))
     }
+  }
+
+  async update(book: IUpdateBookModel): Promise<IBook[]> {
+    let query = "UPDATE books SET"
+    const queryValues: any[] = []
+    let queryParams = 1
+    if (book.book_name) {
+      queryValues.push(book.book_name)
+      query += ` book_name = $${queryParams}`
+      queryParams++
+    }
+
+    if (book.student_name) {
+      queryValues.push(book.student_name)
+      query += queryParams > 1 ? ', ' : ''
+      query += ` student_name = $${queryParams}`
+      queryParams++
+    }
+
+    if (book.student_class) {
+      queryValues.push(book.student_class)
+      query += queryParams > 1 ? ',' : ''
+      query += ` student_class = $${queryParams}`
+      queryParams++
+    }
+
+    if (book.lend_day) {
+      queryValues.push(book.lend_day)
+      query += queryParams > 1 ? ', ' : ''
+      query += ` lend_day = $${queryParams}`
+      queryParams++
+    }
+    
+    query += ` WHERE id = $${queryParams}`
+    queryValues.push(book.id)
+    console.log(query)
+    await PgHelper.query(query, queryValues)
+    return Promise.resolve([])
   }
 }
