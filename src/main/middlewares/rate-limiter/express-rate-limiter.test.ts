8a850c6aea3d08d3aeeeb25f9dbca57e6a40896d
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-05 15:04:24 -0300

    test: ensure ExpressRateLimit return 429 if limit was reach

--- src/main/middlewares/rate-limiter/express-rate-limiter.test.ts
+++ src/main/middlewares/rate-limiter/express-rate-limiter.test.ts
@@ -6,13 +6,13 @@ jest.mock("../auth-middleware/auth-middleware", () => jest.fn((req, res, next) =
 
 describe('ExpressRateLimit', () => {
   it('Should return 200 if limit was not reach', async () => {
-    for (let i = 0; i < 10; i++) {
-      app.get('/test_ratelimit', (req: Request, res: Response) => {
-        res.send({ responde: 'any_response' })
-      })
+    app.get('/test_ratelimit', (req: Request, res: Response) => {
+      res.send({ responde: 'any_response' })
+    })
+    for (let i = 0; i < 101; i++) {
       await request(app)
         .get('/test_ratelimit')
-        .expect(200)
+        .expect(i > 100 ? 429 : 200)
     }
   })
 })
