d788941521ebcbc853dcaf417fe124f18e9a44f7
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-05 15:00:58 -0300

    feat: ensure middlewares call limiter

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -2,9 +2,11 @@ import { Express } from 'express'
 import bodyParser from '../middlewares/body-parser/body-parser'
 import contentType from '../middlewares/content-type/content-type'
 import cors from '../middlewares/cors/cors'
+import { limiter } from '../middlewares/rate-limiter/express-rate-limiter'
 
 export default (app: Express) => {
   app.use(bodyParser)
   app.use(contentType)
   app.use(cors)
+  app.use(limiter)
 }
--- src/main/middlewares/rate-limiter/express-rate-limiter.ts
+++ src/main/middlewares/rate-limiter/express-rate-limiter.ts
@@ -1,11 +1,7 @@
-import { Express } from "express"
 import { rateLimit } from 'express-rate-limit'
 
-export default (app: Express) => {
-  const limiter = rateLimit({
+export const limiter = rateLimit({
     windowMs: 15 * 60 & 1000,
     limit: 100,
     message: 'Limite de requisições atingido. Tente novamente mais tarde.'
   })
-  app.use(limiter)
-}
