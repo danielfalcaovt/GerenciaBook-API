1b6c9c99c10d7536896727cb9a94509f94f49abb
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-13 16:01:14 -0300

    test: ensure AdminLoginController call all dependencies correctly

---
+++ src/presentation/controllers/admin/admin-login.spec.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { HttpRequest, IValidation, badRequest, ok, serverError, unauthorized, } from './admin-login-protocols'
+import { AuthenticationModel, IAuthentication } from '../../../domain/usecases/user-db/authentication'
+import { AdminLoginController } from './admin-login'
+
+interface SutTypes {
+  sut: AdminLoginController
+  validationStub: IValidation
+  authenticationStub: IAuthentication
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const authenticationStub = makeAuthenticationStub()
+  const sut = new AdminLoginController(validationStub, authenticationStub)
+  return {
+    sut,
+    validationStub,
+    authenticationStub
+  }
+}
+
+const makeValidationStub = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(data: any): Error | null {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeAuthenticationStub = (): IAuthentication => {
+  class AuthenticationStub implements IAuthentication {
+    auth(authenticationModel: AuthenticationModel): Promise<string | null> {
+      return new Promise(resolve => resolve('any_token'))
+    }
+  }
+  return new AuthenticationStub()
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+})
+
+describe('AdmLogin Controller', () => {
+  it('Should call validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validationSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(validationSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+  it('Should return an error if validation fail', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error('random_error'))
+    const error = await sut.handle(makeFakeRequest())
+    expect(error).toEqual(badRequest(new Error('random_error')))
+  })
+  it('Should return 500 if validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const error = await sut.handle(makeFakeRequest()) 
+    expect(error).toEqual(serverError())
+  })
+  it('Should call authentication with correct values', async () => {
+    const { sut, authenticationStub } = makeSut()
+    const authSpy = jest.spyOn(authenticationStub, 'auth')
+    await sut.handle(makeFakeRequest())
+    expect(authSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+  it('Should return unauthorized if authentication fail', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const error = await sut.handle(makeFakeRequest())
+    expect(error).toEqual(unauthorized())
+  })
+  it('Should return 500 if authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const error = await sut.handle(makeFakeRequest())
+    expect(error).toEqual(serverError())
+  })
+  it('Should return a token on succeed', async () => {
+    const { sut } = makeSut()
+    const token = await sut.handle(makeFakeRequest())
+    expect(token).toEqual(ok('any_token'))
+  })
+})
