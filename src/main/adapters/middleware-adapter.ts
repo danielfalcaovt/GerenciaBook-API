f0e8fe6b01162840fd3138d2b7f188b52fa31568
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-19 14:55:33 -0300

    feat: ensure MiddlewareAdapter call middleware with correct values

---
+++ src/main/adapters/middleware-adapter.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { IMiddleware } from "../../presentation/protocols/middleware";
+
+export default (middleware: IMiddleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    // interceptação dos usuários que não estão autênticados
+    const response = await middleware.handle(req)
+    console.log(req.headers)
+    if (response.statusCode === 200) {
+      next()
+    } else {
+      return res.status(response.statusCode).json(response.body)
+    }
+  } 
+}
--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import authRoutes from './routes/auth-routes'
 import middlewares from './middlewares'
-import authMiddleware from '../middlewares/auth-middleware/auth-middleware'
 import apiRoutes from './routes/api-routes'
 import adminRoutes from './routes/admin-routes'
 
@@ -9,7 +8,6 @@ const app = express()
 
 middlewares(app)
 authRoutes(app)
-app.use(authMiddleware)
 adminRoutes(app)
 apiRoutes(app)
 
