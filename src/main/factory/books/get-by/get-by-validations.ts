2a8db56e24c651d400f959677de7720f750c1c93
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 13:16:33 -0300

    refactor: formatting code

--- src/main/factory/books/get-by/get-by-validations.ts
+++ src/main/factory/books/get-by/get-by-validations.ts
@@ -1,9 +1,15 @@
-import { RequiredAtLeastOneIn } from "../../../../presentation/helpers/validations/required-at-least-one-in";
-import { IValidation, ValidationComposite } from "./get-by-protocols";
+import { RequiredAtLeastOneIn } from '../../../../presentation/helpers/validations/required-at-least-one-in'
+import { IValidation, ValidationComposite } from './get-by-protocols'
 
 export const makeGetByValidation = (): IValidation => {
   const validations: IValidation[] = []
-  const parameters = ['book_name', 'student_name','student_class' ,'lend_day', 'phone']
+  const parameters = [
+    'book_name',
+    'student_name',
+    'student_class',
+    'lend_day',
+    'phone'
+  ]
   const validation = new RequiredAtLeastOneIn(parameters)
   validations.push(validation)
   return new ValidationComposite(validations)
